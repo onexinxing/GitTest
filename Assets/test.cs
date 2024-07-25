ca4d7d456972926aa384f12f178acb960c1be345
zwk <1933624075@qq.com>
2024-07-25 10:37:58 +0800

    222add

--- Assets/test.cs
+++ Assets/test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    public int a = 2;
+    public int a = 20;
     // Start is called before the first frame update
     void Start()
     {
